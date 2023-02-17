64e2940a44e049be1a6e70befd4c91a18f8a6121
gabriel-victor933 <gabrielvictor933@usp.br>
2023-02-17 10:09:25 -0300

    considerando caracteres especiais

--- src/Jogo.js
+++ src/Jogo.js
@@ -9,8 +9,8 @@ export default function Jogo({imagem, funcao, letras, estado, selecionada, fimJo
             <img data-test="game-image" src = {imagem} alt="forca"></img>
 
             <div>
-            <button data-test="choose-word" onClick={funcao} disabled={estado}>Escolher Palavra</button>
-            <div data-test="word" className={`word ${fimJogo}`} >{letras.map((l) => <span>{selecionada.includes(l) || fimJogo != ""? l : "_"}</span>)}</div>
+            <button data-test="choose-word" onClick={funcao}>Escolher Palavra</button>
+            <div data-test="word" className={`word ${fimJogo}`} >{letras.map((l) => <span>{selecionada.includes(l.normalize('NFD').replace(/[\u0300-\u036f]/g, '')) || fimJogo != ""? l : "_"}</span>)}</div>
             </div> 
 
         </div>
--- src/palavras.js
+++ src/palavras.js
@@ -1,27 +1,7 @@
 const palavras = [
-    "abacate", "abacaxi", "abelha", "abanador", "antologia", "amor", "aba", "abrigo", "abrir",
-    "banana", "boi", "batata", "bacalhau", "bexiga", "bowl", "batedeira", "bisturi", "barreira", "banco",
-    "caixa", "chantilly", "cachorro", "cuidado", "caneta", "carinho", "cabra", "cesto",
-    "dados", "dizer", "danone", "dente", "diamante", "diafragma", "detonar", "dia",
-    "elefante", "esmeralda", "esfinge", "esfera", "encontro", "ema", "escola", "economia",
-    "formiga", "fama", "festa", "fiador", "ferver", "flauta", "figo", "fiapo", "fotografia",
-    "goiaba", "gelo", "grito", "guria", "goleiro", "golfinho", "golfe", "girar",
-    "harmonia", "haste", "hectare", "hepatite", "hiena", "hidrante",
-    "igreja", "importante", "idoso", "irado", "identidade", "idioma", "idade", "idiota",
-    "jantar", "jumento", "jararaca", "janela", "jerimum", "jaula", "jabuti", "jaleco",
-    "laranja", "lua", "luz", "lindo", "lacraia", "lactose", "lacrar",
-    "manga", "morango", "mico", "matar", "mingau", "moqueca", "mocassin",
-    "nectarina", "nada", "navio", "namorado", "ninja", "natal", "narciso", "narina", "nabo",
-    "ovo", "ostra", "obstetra", "orangotango", "olhar", "ortodoxo", "ouro", "ornamento",
-    "pato", "polvo", "povoar", "pano", "princesa", "pizza", "patroa",
-    "queijo", "quitanda", "quinta", "quantia", "quarentena", "quadrilha", "quaresma", "quartzo", "quebrar",
-    "risada", "rio", "remar", "rato", "racional", "rainha", "radioatividade", "raiz", "raiva", "rachadura",
-    "salada", "salamandra", "sacola", "siri", "sabre", "sucarose", "sabedoria", "sacerdote",
-    "tatu", "tabacaria", "taberneiro", "torrada", "tamanho", "tatuagem", "trem",
-    "uva", "universo", "unanimidade", "ubuntu", "universidade", "urso", "uivar", "unir",
-    "vela", "valeta", "vacilo", "valor", "vagem", "vadiagem", "vaca", "valentia", "vidro", "valsa",
-    "xadrez", "xilofone", "xarope", "xenofobia", "xereta", "xerife", "xaveco", "xixi", "xale",
-    "zebra", "zagueiro", "zero", "zoeira", "zepelim", "zinco", "zumbido"
+    "abraço", "ábaco", "comércio", "cupuaçu", "diário", "dromedário", "espátula", "estômago", "esfinge",   "ferver", "fichário", "gamão", "glúten", 
+    "helicóptero","hábito","hemisfério","ícone", "ímpar", "jambú", "jibóia", "leão", "limão", "larápio",  "laço", 
+    "mamão",  "macacão", "maçaneta", "nádega", "oblíquo",  "óculos",  "pólvora", "palhaço", "paróquia", "sábado", "safanão", "tábua", "três","uísque", "união","xícara", "zodíaco", "zangão", "zoológico"
 ]
 
 export default palavras
